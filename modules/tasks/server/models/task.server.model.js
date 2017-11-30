5611c7e33261f84b72ac30b1cc12cc3c78693fa8
Jivan Ghadage <jivanghadage@gmail.com>
2017-11-30 22:07:09 +0530

    private tasks list and creat

--- modules/tasks/server/models/task.server.model.js
+++ modules/tasks/server/models/task.server.model.js
@@ -4,26 +4,35 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 /**
  * Task Schema
  */
 var TaskSchema = new Schema({
-  name: {
-    type: String,
-    default: '',
-    required: 'Please fill Task name',
-    trim: true
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
+    name: {
+        type: String,
+        default: '',
+        required: 'Please fill Task name',
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    deadline: {
+        type: Date
+    },
+    type: {
+        type: String
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
 });
 
 mongoose.model('Task', TaskSchema);
